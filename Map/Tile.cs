e54ebd9d82d19971d1b1b3e368009f7b4e39cb6f
BuildTools <unconfigured@null.spigotmc.org>
2025-01-13 19:57:26 -0800

    Removes unused map logic

--- Map/Tile.cs
+++ Map/Tile.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace SkyTown.Map
 {
-    internal class MapDataModel
+    internal class Tile
     {
-        Vector2 mapDims = new Vector2(0, 0);
+        public bool IsWalkable = true;
+        public Tile()
+        {
+
+        }
     }
 }
